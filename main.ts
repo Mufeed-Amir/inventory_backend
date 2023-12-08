cb908a7cc20668c66d23eaa0dce5cfde04a5b6b3
Mufeed-Amir <mufeed.amir.17290@gmail.com>
2023-12-08 17:29:31 +0530

    Added fetch endpint

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-console.log("HI Its meee...")
+import * as dotenv from 'dotenv'
+const express = require('express');
+
+const app = express ();
+app.use(express.json());
+
+dotenv.config();
+const port = process.env.PORT || 4000;
+
+app.get('/fetch', (request, response) => {
+  
+     response.send("HI Its meee...n");
+  });
+
+app.listen(port, () => {
+     console.log("Server Listening on PORT:", port);
+   });
